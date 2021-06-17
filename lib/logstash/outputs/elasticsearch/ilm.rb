549bd50d5cea7e89339cd1b690f0966a74f5352d
Vijayan Balasubramanian <balasvij@amazon.com>
2021-06-17 16:51:29 -0700

    Fix code review comments
    Signed-off-by: Vijayan Balasubramanian <balasvij@amazon.com>

--- lib/logstash/outputs/elasticsearch/ilm.rb
+++
@@ -1,7 +0,0 @@
-module LogStash; module Outputs; class ElasticSearch
-  module Ilm
-    def ilm_in_use?
-      false
-    end
-  end
-end; end; end
--- spec/integration/outputs/painless_update_spec.rb
+++ spec/integration/outputs/painless_update_spec.rb
@@ -11,9 +11,6 @@ describe "Update actions using painless scripts", :integration => true, :update_
       "hosts" => get_host_port(),
       "action" => "update"
     }
-    if ESHelper.es_version_satisfies?('<6')
-      settings.merge!({"script_lang" => "painless"})
-    end
     LogStash::Outputs::ElasticSearch.new(settings.merge!(options))
   end
 
@@ -75,12 +72,7 @@ describe "Update actions using painless scripts", :integration => true, :update_
 
     context 'with an indexed script' do
       it "should increment a counter with event/doc 'count' variable with indexed script" do
-        if ESHelper.es_version_satisfies?('<6')
-          @es.perform_request(:put, "_scripts/painless/indexed_update", {}, {"script" => "ctx._source.counter += params.event.count" })
-        else
-          @es.perform_request(:put, "_scripts/indexed_update", {}, {"script" => {"source" => "ctx._source.counter += params.event.count", "lang" => "painless"}})
-        end
-
+        @es.perform_request(:put, "_scripts/indexed_update", {}, {"script" => {"source" => "ctx._source.counter += params.event.count", "lang" => "painless"}})
         plugin_parameters = {
           'document_id' => "123",
           'script' => 'indexed_update',
@@ -122,26 +114,6 @@ describe "Update actions using painless scripts", :integration => true, :update_
   end
 
   context "updates with scripted upsert" do
-    if ESHelper.es_version_satisfies?('<6')
-      context 'with file based scripts' do
-        it "should create new documents with upsert content" do
-          subject = get_es_output({ 'document_id' => "456", 'script' => 'scripted_update', 'upsert' => '{"message": "upsert message"}', 'script_type' => 'file' })
-          subject.register
-          subject.multi_receive([LogStash::Event.new("message" => "sample message here")])
-          r = @es.get(:index => 'logstash-update', :type => doc_type, :id => "456", :refresh => true)
-          expect(r["_source"]["message"]).to eq('upsert message')
-        end
-
-        it "should create new documents with event/doc as script params" do
-          subject = get_es_output({ 'document_id' => "456", 'script' => 'scripted_upsert', 'scripted_upsert' => true, 'script_type' => 'file' })
-          subject.register
-          subject.multi_receive([LogStash::Event.new("counter" => 1)])
-          @es.indices.refresh
-          r = @es.get(:index => 'logstash-update', :type => doc_type, :id => "456", :refresh => true)
-          expect(r["_source"]["counter"]).to eq(1)
-        end
-      end
-    end
 
     context 'with an inline script' do
       it "should create new documents with upsert content" do
