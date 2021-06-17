4daf70b809a1f1d3a42ad703f055680971192221
Vijayan Balasubramanian <balasvij@amazon.com>
2021-06-17 09:19:03 -0700

    Remove groovy scripts
    Groovy is supported upto 6.0 versions. Since we only support
    opensource 7.X and OpenSearch. We don't need to test
    groovy scripts.
    
    Signed-off-by: Vijayan Balasubramanian <balasvij@amazon.com>

--- spec/integration/outputs/sniffer_spec.rb
+++ spec/integration/outputs/sniffer_spec.rb
@@ -49,40 +49,6 @@ describe "pool sniffer", :integration => true do
     end
   end
 
-  if ESHelper.es_version_satisfies?("<= 2")
-    describe("Complex sniff parsing ES 2x/1x") do
-      before(:each) do
-        response_double = double("_nodes/http", body: File.read("spec/fixtures/_nodes/2x_1x.json"))
-        allow(subject).to receive(:perform_request).and_return([nil, { version: "2.0" }, response_double])
-        subject.start
-      end
-
-      context "with multiple nodes but single http-enabled data node" do
-        it "should execute a sniff without error" do
-          expect { subject.check_sniff }.not_to raise_error
-        end
-
-        it "should return one sniff URL" do
-          uris = subject.check_sniff
-
-          expect(uris.size).to eq(1)
-        end
-
-        it "should return the correct sniff URL" do
-          if ESHelper.es_version_satisfies?(">= 2")
-            # We do a more thorough check on these versions because we can more reliably guess the ip
-            uris = subject.check_sniff
-
-            expect(uris).to include(::LogStash::Util::SafeURI.new("http://localhost:9201"))
-          else
-            # ES 1.x returned the public hostname by default. This is hard to approximate
-            # so for ES1.x we don't check the *exact* hostname
-            skip
-          end
-        end
-      end
-    end
-  end
 
 
   if ESHelper.es_version_satisfies?(">= 7")
@@ -103,28 +69,6 @@ describe "pool sniffer", :integration => true do
           uris = subject.check_sniff
 
           expect(uris).to include(::LogStash::Util::SafeURI.new("//dev-masterdata:9201"), ::LogStash::Util::SafeURI.new("//dev-data:9202"))
-        end
-      end
-    end
-  end
-  if ESHelper.es_version_satisfies?(">= 5")
-    describe("Complex sniff parsing ES 6x/5x") do
-      before(:each) do
-        response_double = double("_nodes/http", body: File.read("spec/fixtures/_nodes/5x_6x.json"))
-        allow(subject).to receive(:perform_request).and_return([nil, { version: "5.0" }, response_double])
-        subject.start
-      end
-
-      context "with mixed master-only, data-only, and data + master nodes" do
-        it "should execute a sniff without error" do
-          expect { subject.check_sniff }.not_to raise_error
-        end
-
-        it "should return the correct sniff URLs" do
-          # ie. without the master-only node
-          uris = subject.check_sniff
-
-          expect(uris).to include(::LogStash::Util::SafeURI.new("//127.0.0.1:9201"), ::LogStash::Util::SafeURI.new("//127.0.0.1:9202"), ::LogStash::Util::SafeURI.new("//127.0.0.1:9203"))
         end
       end
     end
