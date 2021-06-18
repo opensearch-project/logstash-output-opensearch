6dfadf6847c6ae2164f3c7a98d5061f3cd91b0ea
Vijayan Balasubramanian <balasvij@amazon.com>
2021-06-18 12:57:58 -0700

    Add license headers
    Include SPDX license to all files.
    
    Signed-off-by: Vijayan Balasubramanian <balasvij@amazon.com>

--- spec/integration/outputs/update_spec.rb
+++ spec/integration/outputs/update_spec.rb
@@ -1,3 +1,12 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+#  The OpenSearch Contributors require contributions made to
+#  this file be licensed under the Apache-2.0 license or a
+#  compatible open source license.
+#
+#  Modifications Copyright OpenSearch Contributors. See
+#  GitHub history for details.
+
 require_relative "../../../spec/opensearch_spec_helper"
 
 describe "Update actions without scripts", :integration => true do
