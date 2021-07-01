2a79ad1afd37011b439a409c195d6ddec4c8f052
Vijayan Balasubramanian <balasvij@amazon.com>
2021-07-01 14:30:17 -0700

    Add flag for secure clusters
    Add secure configuration and update service url.
    
    Signed-off-by: Vijayan Balasubramanian <balasvij@amazon.com>

--- scripts/logstash-run.sh
+++ scripts/logstash-run.sh
@@ -5,17 +5,27 @@ export PATH=$BUILD_DIR/gradle/bin:$PATH
 
 SERVICE_URL="http://integration:9200"
 
+if [[ "$SECURE_INTEGRATION" == "true" ]]; then
+  SERVICE_URL="https://integration:9200 -k -u admin:admin"
+fi
+
 wait_for_es() {
   count=120
   while ! curl -s $SERVICE_URL >/dev/null && [[ $count -ne 0 ]]; do
     count=$(( $count - 1 ))
     [[ $count -eq 0 ]] && exit 1
-    sleep 1
+    sleep 20
   done
   echo $(curl -s $SERVICE_URL | python -c "import sys, json; print(json.load(sys.stdin)['version']['number'])")
 }
 
+if [[ "$SECURE_INTEGRATION" == "true" ]]; then
+  extra_tag_args="--tag secure_integration"
+else
+  extra_tag_args="--tag ~secure_integration --tag integration"
+fi
+
 echo "Waiting for cluster to respond..."
 VERSION=$(wait_for_es)
 echo "Integration test cluster $VERSION is Up!"
-bundle exec rspec -fd --tag integration --tag update_tests:painless --tag version:$VERSION spec/integration
+bundle exec rspec -fd $extra_tag_args --tag update_tests:painless --tag version:$VERSION spec/integration
