334d71ed06bc2f02cb14c2d8bd70007131021c5e
Peter Zhu <zhujiaxi@amazon.com>
2021-07-29 16:07:17 -0400

    Add new docker Multi-Arch image building scripts for logstash-output-opensearch (#41)
    * Add new docker building scripts for logstash
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Move build related vars to the top
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

---
+++ release/docker/build-docker-multi-arch.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# This is intended to be run the plugin's root directory. `dockerfiles/run.sh`
+# Ensure you have Docker and docker-compose installed locally
+set -e
+
+# Variables
+BUILDER_NUM=`date +%s`
+BUILDER_NAME="multiarch_${BUILDER_NUM}"
+
+# Imports and functions
+function cleanup_docker_buildx() {
+    # Cleanup docker buildx
+    echo -e "\n* Cleanup docker buildx"
+    docker buildx use default
+    docker buildx rm $BUILDER_NAME > /dev/null 2>&1
+}
+
+# Building plugin
+bash build-plugin.sh
+
+# Identify Logstash version
+version=${LOGSTASH_VERSION}
+if [[ -z "$version" ]]; then
+  version=7.13.2
+fi
+
+# Prepare docker buildx
+trap cleanup_docker_buildx TERM INT EXIT
+echo -e "\n* Prepare docker buildx"
+docker buildx use default
+docker buildx create --name $BUILDER_NAME --use
+docker buildx inspect --bootstrap
+
+# Check buildx status
+echo -e "\n* Check buildx status"
+docker buildx ls | grep $BUILDER_NAME
+docker ps | grep $BUILDER_NAME
+
+# Docker Build Images
+docker buildx build --platform linux/amd64,linux/arm64 --build-arg VERSION=$version -t opensearchstaging/logstash-oss-with-opensearch-output-plugin:$version -f Dockerfile --push .
+
